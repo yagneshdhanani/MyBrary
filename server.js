30db00223e7816ac9d82ec67c07083c2b09249ca
yagnesh791 <yagneshdhanani791@gmail.com>
2021-02-07 23:02:21 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+const indexRouter = require("./routes/index");
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to db"));
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
