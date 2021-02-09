4b886c017924cee4dda1961c0d50323b50ca73dc
yagnesh791 <yagneshdhanani791@gmail.com>
2021-02-09 12:23:10 +0530

    Initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const author = require("../models/author");
+const Author = require("../models/author");
+const router = express.Router();
+
+// All Authors routes
+router.get("/", async (req, res) => {
+  let searchOption = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOption.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOption);
+    res.render("authors/index", {
+      authors: authors,
+      searchOption: req.query.name,
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+// New Authors routes
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create Author routes
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    res.redirect(`authors`);
+  } catch (error) {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error while creating author",
+    });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -25,6 +27,9 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
